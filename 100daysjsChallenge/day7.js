1231e0c5c507edee14c9858273891c1bb47ba43f
wcoder547 <mw5667155@gmail.com>
2025-05-21 20:34:06 +0500

    Add function to find the highest number in an array

---
+++ 100daysjsChallenge/day7.js
@@ -0,0 +1,20 @@
+function highestNumber(arr) {
+  let highest = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (highest < arr[i]) {
+      highest = arr[i];
+    }
+  }
+  return highest;
+}
+
+console.log(highestNumber([1, 45, 11, 22, 567, 0, 9, 7]));
+
+// Output: 567
+//Another Approach
+
+const findMax = (arr) => {
+  return arr.reduce((acc, cur) => {
+    return cur > acc ? cur : acc;
+  }, 0);
+};
